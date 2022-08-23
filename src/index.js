663ddafe71a189ddafcbb7a37b7ec56b022a954d
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-08-23 12:43:00 +0300

    Challange Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let form = document.querySelector(`form`)
+form.addEventListener(`submit`, (e) => {
+e.preventDefault()
+const texts = document.getElementById(`newtask-description`).value;
+buildToDo(texts)
+  form.reset();
+})
+  //your code here...
+ 
+ function buildToDo(todo){
+  let p = document.createElement(`p`);
+    let btn = document.createElement (`button`);
+    btn.addEventListener(`click`,handleDelete);
+  btn.textContent = `   Delete `
+    p.textContent = ` ${todo}`;
+    p.appendChild(btn);
+    console.log(p);
+    document.querySelector(`#list`).appendChild(p)
+  }
+
+  function handleDelete(e){
+    e.target.parentNode.remove();
+  }
+})
